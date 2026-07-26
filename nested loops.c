a901530ea8ffafd859d2c7733acecde6509deb76
allanmocen64 -arch <allanmocen64@gmail.com>
2026-07-26 19:51:50 +0300

    Add as an update

---
+++ nested loops.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int columns = 0;
+  int rows = 0;
+  int columns = 0;
+  char symbol = '\0';
+
+  printf("Enter number of rows: ");
+  scanf("%d", &rows);
+
+  printf("Enter number of columns: ");
+  scanf("%d", &columns);
+
+  printf("Enter type of symbol: ");
+  scanf(" %c", &symbol);
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int i = 0; i < columns; i++)
+    {
+      printf("%c", symbol);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
